be4af2d00ee1a696e6e49ccb7b6befcb0606eb29
dsourav155 <work.sourav155@gmail.com>
2024-08-05 17:00:43 +0530

    first java code pushed to github

---
+++ src/practice_java/Main.java
@@ -0,0 +1,28 @@
+package practice_java;
+
+// class
+public class Main {
+
+	// main method
+	public static void main(String[] args) {
+		
+		//print statement
+		System.out.println("Let's practice java!!");
+		// ( \n: next line, \t: tab, \"\": quotes, etc. )
+		System.out.println("\"Let's Go!\"");
+		
+		// use "sysout" with ^ + space to write System.out.println();
+		
+		// This is a single line comment
+		
+		/* 
+		 * This
+		 * is
+		 * a
+		 * multi-line
+		 * comment
+		 */
+
+	}
+
+}

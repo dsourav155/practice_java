c3562522ed011ab8d6676b8e3e250b7e83828c8e
dsourav155 <work.sourav155@gmail.com>
2024-08-05 18:37:28 +0530

    How to define variables in java

--- src/practice_java/Main.java
+++ src/practice_java/Main.java
@@ -1,15 +1,15 @@
+  // ----------------------------- Syntax -------------------------------
+
+
 package practice_java;
 
-// class
-public class Main {
+public class Main {  // class
 
-	// main method
-	public static void main(String[] args) {
+	public static void main(String[] args) {  // main method
 		
-		//print statement
-		System.out.println("Let's practice java!!");
-		// ( \n: next line, \t: tab, \"\": quotes, etc. )
-		System.out.println("\"Let's Go!\"");
+		System.out.println("Let's practice java!!"); //print statement
+		
+		System.out.println("\"Let's Go!\"");  // ( \n: next line, \t: tab, \"\": quotes, etc. )
 		
 		// use "sysout" with ^ + space to write System.out.println();
 		
@@ -22,6 +22,37 @@ public class Main {
 		 * multi-line
 		 * comment
 		 */
+		
+		
+	//	------------------------------ Variables -------------------------------
+		
+		int x; //declaration
+		
+		x = 130; //assignment 
+		
+		int y = 15; //initialization
+		
+		Long z = 1721237194623841L; // large numbers
+		
+		float a = 3.3245523f;  //decimal, use double for large decimal 
+		
+		boolean b = true; // true or false
+		
+		String c = "Sourav"; // use double quotes " "
+		
+		char d = '@'; // any character or symbol, use single quotes ''
+		
+		
+		
+		System.out.println(x);
+		System.out.println("My lucky number is "+y);
+		System.out.println("This is my phone number "+ z);
+		System.out.println("What is divisible by "+a);
+		System.out.println("Is 1 a prime numer? "+b);
+		System.out.println("My name is "+c);
+		System.out.println("My LinkedIn is "+d+"dsourav155");
+		
+		
 
 	}
 
